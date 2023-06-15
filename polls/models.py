65596f9e91f18c1069286e1faa0260a5abec65c9
abhijitotta17 <abhijitotta17@gmail.com>
2023-06-16 01:01:11 +0530

    first

---
+++ polls/models.py
@@ -0,0 +1,49 @@
+'''from django.db import models
+from django_tenants.models import TenantMixin, DomainMixin
+
+class Client(TenantMixin):
+    name = models.CharField(max_length=100)
+    paid_until =  models.DateField()
+    on_trial = models.BooleanField()
+    created_on = models.DateField(auto_now_add=True)
+
+    # default true, schema will be automatically created and synced when it is saved
+    auto_create_schema = True
+
+class Domain(DomainMixin):
+    pass'''
+'''from django.db import models
+from tenant_schemas.models import TenantMixin
+
+
+class Client(TenantMixin):
+    name = models.CharField(max_length=100)'''
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Poll(models.Model):
+    question = models.CharField(max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, related_name="choices", on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, related_name="votes", on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    voted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("poll", "voted_by")
